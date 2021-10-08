a0b1ef45d123df04837403ae52026037c95fba58
solo7602@mail.ru <solo7602@mail.ru>
2021-10-08 12:53:11 +0400

    com

--- labaBuldozerKazakovISEbd-22/labaBuldozerKazakovISEbd-22/FormBulldozer.cs
+++ labaBuldozerKazakovISEbd-22/labaBuldozerKazakovISEbd-22/FormBulldozer.cs
@@ -12,7 +12,7 @@ namespace labaBuldozerKazakovISEbd_22
 {
 	public partial class FormBulldozer : Form
 	{
-		private Bulldozer car;
+		private IBuldozer buldozer;
 		public FormBulldozer()
 		{
 			InitializeComponent();
@@ -21,16 +21,25 @@ namespace labaBuldozerKazakovISEbd_22
 		{
 			Bitmap bmp = new Bitmap(Picture.Width, Picture.Height);
 			Graphics gr = Graphics.FromImage(bmp);
-			car.DrawTransport(gr);
+			buldozer.DrawTransport(gr);
 			Picture.Image = bmp;
 		}
-		private void buttonCreate_Click(object sender, EventArgs e)
+		private void button_bul_Click_1(object sender, EventArgs e)
 		{
 			Random rnd = new Random();
-			car = new Bulldozer();
-			car.Init(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue,
-		   Color.Yellow, true, true, true); car.SetPosition(rnd.Next(100, 200),
-		   rnd.Next(100, 200), Picture.Width, Picture.Height);
+			buldozer = new BuldozerBase(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue);
+			buldozer.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), Picture.Width,
+Picture.Height);
+			Draw();
+		}
+
+		private void button_mod_Click_1(object sender, EventArgs e)
+		{
+			Random rnd = new Random();
+			buldozer = new ModBuldozer(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue,
+		   Color.Yellow, true, true);
+			buldozer.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), Picture.Width,
+		   Picture.Height);
 			Draw();
 		}
 		private void buttonMove_Click(object sender, EventArgs e)
@@ -40,21 +49,19 @@ namespace labaBuldozerKazakovISEbd_22
 			switch (name)
 			{
 				case "buttonUp":
-					car.MoveTransport(Direction.Up);
+					buldozer.MoveTransport(Direction.Up);
 					break;
 				case "buttonDown":
-					car.MoveTransport(Direction.Down);
+					buldozer.MoveTransport(Direction.Down);
 					break;
 				case "buttonLeft":
-					car.MoveTransport(Direction.Left);
+					buldozer.MoveTransport(Direction.Left);
 					break;
 				case "buttonRight":
-					car.MoveTransport(Direction.Right);
+					buldozer.MoveTransport(Direction.Right);
 					break;
 			}
 			Draw();
 		}
-
-
 	}
 }
